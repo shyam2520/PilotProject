df7e792c810b2d9530408dacb4168cbbbb77a0f1
sanjaymaniam <sanjaysubramaniam99@gmail.com>
2021-06-24 21:56:36 +0530

    header and basic search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppComponent } from './app.component';
 // https://mdbootstrap.com/docs/angular/getting-started/installation/
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
